979af4a2018865fcf67c499b42adaef0029ed3ec
Anton Neld <antonherrneld@gmail.com>
2019-09-23 19:33:36 +0200

    Basic component for the live screen. (#7)
    Basic component for the live screen.
    
    Right now we only display the number of times
    the space bar has been pressed, to exercise
    handling key presses in some way.

--- src/tsx/main.tsx
+++ src/tsx/main.tsx
@@ -1,13 +1,7 @@
-
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { LiveScreen } from "./liveScreen";
 
 import "../less/main.less";
 
-// Temporarily allow console until we have more going on.
-/* tslint:disable no-console */
-console.log("Hello world!");
-
-console.log(React);
-console.log(ReactDOM);
-/* tslint:enable no-console */
+ReactDOM.render(<LiveScreen />, document.getElementById("super-container"));
