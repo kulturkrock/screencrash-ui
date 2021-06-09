bcb8035b687a05c6fcf2b9437df9e1e36554ae3a
Anton Neld <antonherrneld@gmail.com>
2021-06-09 18:56:43 +0200

    Make the PDF viewer take up the available space

--- src/tsx/pdfViewer.tsx
+++ src/tsx/pdfViewer.tsx
@@ -3,16 +3,38 @@ import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
 
 import style from "../less/pdfViewer.module.less";
 
-class PdfViewer extends React.PureComponent<{ script: string }> {
+interface IState {
+  id: string;
+  pdfWidth: number;
+}
+
+class PdfViewer extends React.PureComponent<{ script: string }, IState> {
   constructor(props: { script: string }) {
     super(props);
+    // Create a random ID, to avoid collisions if we ever have multiple
+    // PDF viewers.
+    this.state = {
+      id: `pdfViewer${Math.round(Math.random() * 10000000)}`,
+      pdfWidth: 0,
+    };
+  }
+
+  public componentDidMount(): void {
+    const { width } = document
+      .getElementById(this.state.id)
+      .getBoundingClientRect();
+    // This will only grow the container, never shrink it.
+    // Since we only do it when mounting it's fine for now.
+    this.setState({
+      pdfWidth: width,
+    });
   }
 
   public render(): JSX.Element {
     return (
-      <div className={style.container}>
+      <div className={style.container} id={this.state.id}>
         <Document file={this.props.script}>
-          <Page pageNumber={1} />
+          <Page pageNumber={1} width={this.state.pdfWidth} />
         </Document>
       </div>
     );
