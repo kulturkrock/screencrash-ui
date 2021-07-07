47adc9ba3bf3a9fa62cda36b103d5e4824e08940
Anton Neld <antonherrneld@gmail.com>
2021-07-07 19:51:29 +0200

    Scroll to the current line when switching

--- src/tsx/pdfViewer.tsx
+++ src/tsx/pdfViewer.tsx
@@ -3,32 +3,66 @@ import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
 
 import style from "../less/pdfViewer.module.less";
 
+interface IProps {
+  script: string;
+  currentPage: number;
+  currentLocationOnPage: number;
+}
+
 interface IState {
   id: string;
   pdfWidth: number;
+  pdfHeight: number;
   numPages: number;
+  loadedPages: number; // To keep track of when we've fully loaded
 }
 
-class PdfViewer extends React.PureComponent<{ script: string }, IState> {
-  constructor(props: { script: string }) {
+class PdfViewer extends React.PureComponent<IProps, IState> {
+  constructor(props: IProps) {
     super(props);
     // Create a random ID, to avoid collisions if we ever have multiple
     // PDF viewers.
     this.state = {
       id: `pdfViewer${Math.round(Math.random() * 10000000)}`,
       pdfWidth: 0,
+      pdfHeight: 0,
       numPages: 0,
+      loadedPages: 0,
     };
   }
 
   public componentDidMount(): void {
-    const { width } = document
+    const { width, height } = document
       .getElementById(this.state.id)
       .getBoundingClientRect();
     // This will only grow the container, never shrink it.
     // Since we only do it when mounting it's fine for now.
     this.setState({
       pdfWidth: width,
+      pdfHeight: height,
+    });
+  }
+
+  public componentDidUpdate(prevProps: IProps, prevState: IState): void {
+    if (
+      this.props.currentPage !== prevProps.currentPage ||
+      this.props.currentLocationOnPage !== prevProps.currentLocationOnPage ||
+      (this.state.numPages !== 0 &&
+        this.state.loadedPages !== prevState.loadedPages &&
+        this.state.loadedPages === this.state.numPages)
+    ) {
+      this.scrollToLocation(
+        this.props.currentPage,
+        this.props.currentLocationOnPage,
+      );
+    }
+  }
+
+  private scrollToLocation(page: number, locationOnPage: number): void {
+    const pageHeight = this.state.pdfWidth * Math.sqrt(2); // Assume A4 size
+    document.getElementById(this.state.id).scrollTo({
+      top: (page + locationOnPage) * pageHeight - 0.5 * this.state.pdfHeight,
+      behavior: "smooth",
     });
   }
 
@@ -48,6 +82,9 @@ class PdfViewer extends React.PureComponent<{ script: string }, IState> {
               width={this.state.pdfWidth}
               renderAnnotationLayer={false}
               renderTextLayer={false}
+              onLoadSuccess={() => {
+                this.setState({ loadedPages: this.state.loadedPages + 1 });
+              }}
             />
           ))}
         </Document>
